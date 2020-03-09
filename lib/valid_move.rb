7c2cd8fb48609fa8bf47cbf471f5d78dbc752c11
Nguyen Van Say <vansay@hotmail.fr>
2020-03-09 16:34:24 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index_number)
-  if position_taken?(board, index_number)
-  
+  if position_taken?(board, index_number) == true
+    false
+  else
+    false
   end
 end
 
