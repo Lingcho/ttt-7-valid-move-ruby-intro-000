27a971c8a7eae8231e6d967c59cc297c373463a6
Nguyen Van Say <vansay@hotmail.fr>
2020-03-09 16:09:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,8 +13,6 @@ def position_taken?(board, index_number)
   false
   elsif position == " " || position == "" || position == nil
     false
-  elsif index_number.between?(1, 9)
-    false
   elsif position == "X" || position == "O"
     true
   end
